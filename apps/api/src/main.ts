a05beacda1a8241a990b660b860f9e099f8a2beb
Uygar Uyaniksoy <uygar.uyaniksoy@dynatrace.com>
2021-12-04 14:26:37 +0100

    deploy scripts

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,11 +1,19 @@
 import * as express from 'express';
+import * as path from 'path';
 import { setupMongo } from './mongo';
 import { router } from './routes';
 
 const app = express();
+const CLIENT_BUILD_PATH = path.join(__dirname, '../getir-todo');
+
+app.use(express.static(CLIENT_BUILD_PATH));
 
 app.use(express.json());
 app.use('/api', router);
+
+app.get('*', (request, response) => {
+  response.sendFile(path.join(CLIENT_BUILD_PATH, 'index.html'));
+});
 
 setupMongo();
 
