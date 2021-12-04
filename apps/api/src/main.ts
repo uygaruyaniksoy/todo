59d5a53d27457e0d3dad30d50016e2cd924a6fd8
Uygar Uyaniksoy <uygar.uyaniksoy@dynatrace.com>
2021-12-04 14:41:40 +0100

    change port env var

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -17,7 +17,7 @@ app.get('*', (request, response) => {
 
 setupMongo();
 
-const port = process.env.port || 3333;
+const port = process.env.PORT || 3333;
 const server = app.listen(port, () => {
   console.log('Listening at http://localhost:' + port + '/api');
 });
